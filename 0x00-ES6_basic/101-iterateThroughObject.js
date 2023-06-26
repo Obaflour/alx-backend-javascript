3f4cd2aa52d10408e690c8cc9bef67a9fe54be2e
Obafuwa Olayemi <obafuwaolayemi@gmail.com>
2023-06-26 20:23:52 +0000

    the end

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return
- }
+  let result = '';
+
+  for (const employee of reportWithIterator) {
+    result += employee + ' | ';
+  }
+
+  // Remove the trailing ' | ' from the result
+  result = result.slice(0, -3);
+
+  return result;
+}
