c3a45091e04e18cce51ef6bdc40ff2417e64e266
Obafuwa Olayemi <obafuwaolayemi@gmail.com>
2023-06-26 19:18:52 +0000

    modification

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+function taskFirst() {
+  const firstName = "Olayemi";
+  const lastName = "Obafuwa";
+
+  console.log(`First Name: ${firstName}`);
+  console.log(`Last Name: ${lastName}`);
+}
+
+function taskNext() {
+  let age = 25;
+  let profession = "Engineer";
+
+  console.log(`Age: ${age}`);
+  console.log(`Profession: ${profession}`);
+}
+
